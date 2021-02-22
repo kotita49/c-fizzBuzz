a3281c2f52053d6ff0433671376c8efb5791025e
kotita49 <kotita49@gmail.com>
2021-02-22 12:22:30 +0000

    added Bong

--- Program.cs
+++ Program.cs
@@ -9,12 +9,15 @@ namespace c_FizzBuzz
             for (int i = 1; i <= 100; i++)  
 { 
     string print = "";
-                 if(i%3==0){
-                     print += "Fizz";
+    
+    if(i%3==0){
+     print += "Fizz";
             } if(i%5==0){
                 print += "Buzz";
             }if(i%7==0){
                 print+= "Bang";
+            }if(i%11==0){
+        print = "Bong";
             }
             if(print.Length == 0){
                 print = i.ToString();
