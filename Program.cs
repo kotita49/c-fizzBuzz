0d8360d71b3f693bec13fdfe2015ac080b714a7c
kotita49 <kotita49@gmail.com>
2021-02-22 10:53:40 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
